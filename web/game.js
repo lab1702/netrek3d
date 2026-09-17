e4d416ab55df4e93ec2dcdcf3e46d2c8b4460607
lab1702 <lab1702@gmail.com>
2026-09-16 23:22:27 -0400

    Show X and Y alongside Z in the ship dashboard

--- web/game.js
+++ web/game.js
@@ -417,7 +417,7 @@ function bar(label, val, max, warnHigh, color, text) {
 function updateHUD(you, players) {
   const compass = ((you.d * 180 / Math.PI + 90) % 360 + 360) % 360;
   hudLeft.innerHTML =
-    `Z ${Math.round(you.z)} &nbsp; PITCH ${Math.round(you.pitch * 180 / Math.PI)}&deg;<br>` +
+    `X ${Math.round(you.x)} &nbsp; Y ${Math.round(you.y)} &nbsp; Z ${Math.round(you.z)} &nbsp; PITCH ${Math.round(you.pitch * 180 / Math.PI)}&deg;<br>` +
     bar("WARP", you.sp, you.maxsp, false, "var(--amber)", `${you.sp}/${you.maxsp}`) +
     bar("SHLD", you.sh, you.maxsh) +
     bar("HULL", you.maxdm - you.dm, you.maxdm) +
